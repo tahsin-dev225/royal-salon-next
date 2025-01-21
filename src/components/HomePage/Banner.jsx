82a3d4ae445f1907a625173e1958bc71958e956d
tahsin-dev225 <ahmedtasin225@gmail.com>
2025-01-21 18:49:54 +0600

    fifth 1

--- src/components/HomePage/Banner.jsx
+++ src/components/HomePage/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
     return (
         <div className="3xl:container  bg-[url('/img/slBanner.jpg')] w-full bg-cover relative mx-auto max-h-[800px]">
             <div className="w-full h-[300px] p-3 flex items-center justify-center flex-col lg:py-20 lg:h-screen max-h-[800px]  top-0  bg-black bg-opacity-60">
-                <h3 className="text-2xl font-mono text-sky-50">Fell the premiumness.<FaArrowRight /></h3>
+                <h3 className="text-2xl font-mono text-sky-50">Fell the premiumness.</h3>
                 <h1 className="text-2xl lg:my-3 lg:text-6xl text-center font-serif font-semibold ">
                     <span className="">Wellcome to  </span>
                     <span className="bg-gradient-to-br from-[#b5c54f] mr-3 to-[#c18f44fa]  bg-clip-text text-transparent"> Royal Style</span>
@@ -17,7 +17,7 @@ const Banner = () => {
                 <p className="font-light text-[15px] text-center text-slate-100 lg:leading-10">Experience luxury, elegance, and sophistication at Royal Style Salon, where beauty meets artistry.</p>
                 <Link  className="py-2 px-6 hover:bg-orange-400  flex justify-center items-center gap-2 bg-gradient-to-l from-[#7d502ea1] delay-200 transition-all border-[1px]  to-[#f59f0b6e] border-[#ba8f53] rounded-[3px] my-5 njr"
                  href={'/services'}>
-                    Make A Apointment 
+                    Make A Apointment <FaArrowRight />
                 </Link>
             </div>
         </div>
